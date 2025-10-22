3b7f897e0a9fd426a4ca82cfeff0aad12aa9c648
b4r0n <gerardo.ruiz@kaseya.com>
2025-10-22 13:16:24 +0200

    remove prints

--- Payload_Type/thanatos/thanatos/agent_code/src/main.rs
+++ Payload_Type/thanatos/thanatos/agent_code/src/main.rs
@@ -2,8 +2,5 @@ use thanatos::real_main;
 
 /// Entrypoint when running the binary standalone.
 fn main() {
-    if let Err(e) = real_main() {
-        eprintln!("Fatal error: {}", e);
-        std::process::exit(1);
-    }
+    real_main().unwrap();
 }
